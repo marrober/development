58cb2f38ee7b96d5af14e997fc54e560db291608
Mark Roberts <marrober@redhat.com>
2022-09-15 11:58:03 +0100

    Adding network policy and Jenkins server config

--- openshift/network-policy/asset-creation.sh
+++ openshift/network-policy/asset-creation.sh
@@ -1,22 +1,34 @@
 oc new-project left
 oc label namespace left projectName=left
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=first --env=NEXT_LAYER_NAME=second --env=NEXT_LAYER_NAMESPACE=left --env=THIS_LAYER_NAME=first --env=VERSION_ID=v1 --namespace=left
+oc create secret generic github-access -n left \
+ --from-literal=username=marrober \
+ --from-literal=password=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
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=second --env=NEXT_LAYER_NAME=third --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=second --env=VERSION_ID=v1 --namespace=left
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=first --env=NEXT_LAYER_NAME=second --env=NEXT_LAYER_NAMESPACE=left --env=THIS_LAYER_NAME=first --env=VERSION_ID=v1 --namespace=left --source-secret=github-access
+
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=second --env=NEXT_LAYER_NAME=third --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=second --env=VERSION_ID=v1 --namespace=left --source-secret=github-access
 
 oc expose svc/first --namespace left
 
 oc new-project right
 oc label namespace right projectName=right
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=third --env=NEXT_LAYER_NAME=fourth --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=third --env=VERSION_ID=v1 --namespace=right
+oc create secret generic github-access -n right \
+ --from-literal=username=marrober \
+ --from-literal=password=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
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=fourth --env=THIS_LAYER_NAME=fourth --env=VERSION_ID=v1 --namespace=right
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=third --env=NEXT_LAYER_NAME=fourth --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=third --env=VERSION_ID=v1 --namespace=right --source-secret=github-access
+
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=fourth --env=THIS_LAYER_NAME=fourth --env=VERSION_ID=v1 --namespace=right --source-secret=github-access
 
 oc new-project rogue
 oc label namespace rogue projectName=rogue
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=rogue --env=NEXT_LAYER_NAME=third --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=rogue
+oc create secret generic github-access -n rogue \
+ --from-literal=username=marrober \
+ --from-literal=password=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
+
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=rogue --env=NEXT_LAYER_NAME=third --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=rogue --source-secret=github-access
 
 oc expose svc/rogue --namespace rogue
