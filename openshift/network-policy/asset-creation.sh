42c15e05054b9b9bdd2893ff38ee8c074697c79d
Mark Roberts <marrober@redhat.com>
2024-10-01 11:12:29 +0100

    update

--- openshift/network-policy/asset-creation.sh
+++ openshift/network-policy/asset-creation.sh
@@ -1,33 +1,30 @@
 # Add the github access token before executing.
 
-oc new-project left
-oc label namespace left projectName=left
+oc process -f project-template.yaml -p PROJECT_NAME=left -p ASPECT=network-security | oc create -f -
 
-oc create secret generic github-access -n left \
+oc create secret generic github-access -n left --type=kubernetes.io/basic-auth \
  --from-literal=username=marrober \
- --from-literal=password=
+ --from-literal=password=$1
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=one --env=NEXT_LAYER_NAME=two --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=one --env=VERSION_ID=v1 --namespace=left 
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --source-secret=github-access --name=one --env=NEXT_LAYER_NAME=two --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=one --env=VERSION_ID=v1 --namespace=left 
 
 oc expose svc/one --namespace left
 
-oc new-project right
-oc label namespace right projectName=right
+oc process -f project-template.yaml -p PROJECT_NAME=right -p ASPECT=network-security | oc create -f -
 
-oc create secret generic github-access -n right \
+oc create secret generic github-access -n right --type=kubernetes.io/basic-auth \
  --from-literal=username=marrober \
- --from-literal=password=
+ --from-literal=password=$1
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=two --env=THIS_LAYER_NAME=two --env=VERSION_ID=v1 --namespace=right --source-secret=github-access
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --source-secret=github-access --name=two --env=THIS_LAYER_NAME=two --env=VERSION_ID=v1 --namespace=right --source-secret=github-access
 
-oc new-project rogue
-oc label namespace rogue projectName=rogue
+oc process -f project-template.yaml -p PROJECT_NAME=rogue -p ASPECT=network-security | oc create -f -
 
-oc create secret generic github-access -n rogue \
+oc create secret generic github-access -n rogue --type=kubernetes.io/basic-auth \
  --from-literal=username=marrober \
- --from-literal=password=
+ --from-literal=password=$1
 
-oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --name=rogue --env=NEXT_LAYER_NAME=two --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=rogue
+oc new-app nodejs~https://github.com/marrober/development.git#main --context-dir=openshift/network-policy --source-secret=github-access --name=rogue --env=NEXT_LAYER_NAME=two --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=rogue
 
 oc expose svc/rogue --namespace rogue
 
