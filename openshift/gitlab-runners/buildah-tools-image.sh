dc9166edecba2c7d17879c45a954af95059b515d
marrober <marrober@redhat.com>
2025-12-18 16:28:54 +0000

    update

--- openshift/gitlab-runners/buildah-tools-image.sh
+++ openshift/gitlab-runners/buildah-tools-image.sh
@@ -1,2 +1,2 @@
-buildah bud -f buildah-tools-image -t quay.io/marrober/buildah-for-gitlab-runners:4
-buildah push quay.io/marrober/buildah-for-gitlab-runners:4
+buildah bud -f buildah-tools-image -t quay.io/marrober/buildah-for-gitlab-runners:6
+buildah push quay.io/marrober/buildah-for-gitlab-runners:6
